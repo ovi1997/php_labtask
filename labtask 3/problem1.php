e1466782ababcb88f16f8cfa96b9d526a1fbc6cc
ovi1997 <77160526+ovi1997@users.noreply.github.com>
2021-02-08 12:49:06 +0600

    labtask 3 done

---
+++ labtask 3/problem1.php
@@ -0,0 +1,7 @@
+<html>
+<head></head>
+<body> A quick brown <b> fox </b> jumps over the lazy echo <b> dog </b>
+</body>
+
+
+
---
+++ labtask 3/problem2.php
@@ -0,0 +1,13 @@
+<?php
+$marks=90;
+if {
+    $marks >=90 : A+}
+    $marks >80 and <90: A
+    $marks >70 and <80: B
+    $marks >60 and <70 : C
+}
+else {
+    the grade is : F
+} 
+
+?>
