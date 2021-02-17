0e11abdce96db9e2dfc9c38c8e2581d88900fe3c
ovi1997 <77160526+ovi1997@users.noreply.github.com>
2021-02-17 19:08:35 +0600

    dggdfuy.php

---
+++ New folder/table.php
@@ -0,0 +1,69 @@
+<html>
+<head></head>
+<body>
+<!--    <table border="1">
+         <tr>
+              <td colspan="2">group alibaba.com</td>
+         </tr>
+         <tr>
+              <td><b>name</b></td>
+              <td><b>id</b></td>
+              <td><b>cgpa</b></td>
+         </tr>
+
+         <tr>
+             <td rowspan="2">ovi</td>
+             <td>12</td>
+             <td>2.50</td>
+         </tr>
+
+         <tr>
+             
+             <td>13</td>
+             <td>3.50</td>
+         </tr>
+
+         <tr>
+             <td rowspan="2">kotha</td>
+             <td>14</td>
+             <td>2.90</td>  
+         </tr>
+         <tr>
+             <td>18</td>
+             <td>3.45</td>  
+         </tr>
+
+    </table>-->
+
+<h1>User Interface</h1>
+<form action="" method="">
+     <span>username :</span> <input type="text"><br>
+     <span>password :</span> <input type="password"><br>
+     <span>Gender   :</span> <input type="radio" name="gender">male <input type="radio" name="gender">female<br>
+     <span>Hobbies  :</span> <input type="checkbox">movie  <input type="checkbox">garden  <input type="checkbox">cinema  <input type="checkbox">coading<br>
+     <span>profession :</span>
+     <select>
+           <option>teaching</option>
+           <option>cultivating</option>
+           <option>farming</option>
+           <option>gv work</option>
+           <option>shipping</option>
+           <option>doctor</option>
+           <option>engineer</option>
+           <option>architecture</option>
+     </select><br>
+     <input type="submit" value="submit">
+</form>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
